9061f8c83df6d9dfd461896291a933886f186270
KlaudiuszB5528 <klaudiusz.biegacz@gmail.com>
2023-08-24 00:24:10 +0200

    centered clerk component

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return <div className="w-screen h-screen grid place-items-center">
+      <SignUp />
+    </div>
 }
